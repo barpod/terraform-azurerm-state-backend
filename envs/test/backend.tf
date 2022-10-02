7e6c6a8228c2a73071c88759395b7af5ec555819
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2022-10-02 16:54:46 +0200

    Ongoing work

--- envs/test/backend.tf
+++ envs/test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   # backend "azurerm" {
   #     resource_group_name  = "rg_test_tf_remote_state"
-  #     storage_account_name = "tfazurebarpodremotestatetest"
+  #     storage_account_name = "tfbarpodremotestatetest"
   #     container_name       = "terraform-state"
   #     key                  = "terraform.tfstate"
   # }
--- envs/test/terraform.tfvars
+++ envs/test/terraform.tfvars
@@ -1,3 +1,4 @@
 location="West Europe"
 resource_group_name="rg_test_tf_remote_state"
 storage_account_name="tfbarpodremotestatetest"
+storage_container_name="terraform-state"
