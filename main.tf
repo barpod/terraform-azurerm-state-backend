83d6bccda4fd87f6bf37b30121ad9b7c2b5192fe
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2022-10-02 12:55:32 +0200

    Migrate to remote state

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.25.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "rg_tf_remote_state"
+        storage_account_name = "tfazurebarpodremotestate"
+        container_name       = "terraform-state"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
 provider "azurerm" {
   features {}
 }
